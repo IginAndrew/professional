aaf4a4f14f83eda161ae89eb7845af6a48cafe7e
asigi <asigin79@gmail.com>
2024-12-21 19:44:19 +0300

    add request_file.py

---
+++ request_file.py
@@ -0,0 +1,6 @@
+import requests
+def date():
+    url = "https://andrew79.pythonanywhere.com/download/python_request.zip"
+    response = requests.get(url)
+    with open("python_request.zip", "wb") as code:
+        code.write(response.content)
