075278dd290ab3de8a55969e0cb4c28114028f0e
asigi <asigin79@gmail.com>
2024-12-21 20:10:06 +0300

    add request_file.py

--- request_file.py
+++ request_file.py
@@ -1,11 +1,11 @@
 import requests
 
 
-def date():
+def data():
     url = "https://andrew79.pythonanywhere.com/download/python_request.zip"
     response = requests.get(url)
     with open("python_request.zip", "wb") as code:
         code.write(response.content)
 
 if __name__ == "__main__":
-    date()
+    data()
