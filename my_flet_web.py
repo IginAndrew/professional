c65afb4a4c887d3d1018b770386f6f35aa60d8d9
IginAndrew <asigin79@gmail.com>
2025-01-09 10:39:23 +0300

    add новости

--- my_flet_web.py
+++ my_flet_web.py
@@ -4,56 +4,119 @@ from datetime import date
 from my_db.select_db import select_user
 
 
-def card(name:str,email:str,phonenumber:str, post_name:str,birthday:str):
+def card(name: str, email: str, phonenumber: str, post_name: str, birthday: str):
     c = ft.Card(
-                ft.Column(
-                        [
-
-                        ft.Text(name, style=ft.TextStyle(color="white", size=15)),
-
-                        ft.Text(post_name, style=ft.TextStyle(color="white", size=10)),
-
-                        ft.Text(email, style=ft.TextStyle(color="white", size=10)),
-
-                        ft.Text(phonenumber, style=ft.TextStyle(color="white", size=10)),
-
-                        ft.Text(birthday[:birthday.index(" ")], style=ft.TextStyle(color="white", size=10)),
-                        ],
+        ft.Column(
+            [
+                ft.Text(name, style=ft.TextStyle(color="white", size=15)),
+                ft.Text(post_name, style=ft.TextStyle(color="white", size=10)),
+                ft.Text(email, style=ft.TextStyle(color="white", size=10)),
+                ft.Text(phonenumber, style=ft.TextStyle(color="white", size=10)),
+                ft.Text(
+                    birthday[: birthday.index(" ")],
+                    style=ft.TextStyle(color="white", size=10),
                 ),
-                color="green",
-                )
+            ],
+        ),
+        color="green",
+    )
 
     return c
 
+
+def card_news(name: str, date: str, text: str):
+    c = ft.Card(
+        ft.Column(
+            [
+                ft.Text(name, style=ft.TextStyle(color="white", size=15)),
+                ft.Text(text, style=ft.TextStyle(color="white", size=10)),
+                ft.Text(date, style=ft.TextStyle(color="white", size=10)),
+                ft.Button(text="добавить в календарь"),
+            ],
+        ),
+        color="green",
+    )
+
+    return c
+
+
+def card_big_news(name: str, date: str, text: str):
+    c = ft.Card(
+        ft.Column(
+            [
+                ft.Text(name, style=ft.TextStyle(color="white", size=15)),
+                ft.Text(text, style=ft.TextStyle(color="white", size=10)),
+                ft.Text(date, style=ft.TextStyle(color="white", size=10)),
+            ],
+        ),
+        color="green",
+    )
+
+    return c
+
+
 card_total_user = [
-        card(
-            name=i["name"],
-            email=i["email"],
-            phonenumber=i["phonenumber"],
-            post_name=i["post_name"],
-            birthday=i["birthday"],
-        )
-        for i in select_user()
-    ]
+    card(
+        name=i["name"],
+        email=i["email"],
+        phonenumber=i["phonenumber"],
+        post_name=i["post_name"],
+        birthday=i["birthday"],
+    )
+    for i in select_user()
+]  # изменить на внешний API
+
+card_total_news = [
+    card_news(
+        "Общее совещание в актовом зале",
+        "26.05.2024",
+        "Все сотрудника отдела 'Администраторы' собираемся",
+    )
+    for _ in range(10)
+]  # изменить на внешний API
+
+
+card_total_big_news = [
+    ft.Container(
+        alignment=ft.alignment.bottom_left,
+        width=450,
+        height=300,
+        bgcolor="#d9d9d9",
+        margin=20,
+        content=ft.Container(
+            alignment=ft.alignment.bottom_left,
+            width=450,
+            height=100,
+            content=card_big_news(
+                "Водители на трассе М-12 сыграли 'Полёт шмеля'",
+                "04.05.2024",
+                """Они ехали-ехали и сыграли! Они ехали-ехали и сыграли! Они ехали ехали и сыграли! Они ехали-ехали и сыграли! Они ехали-ехали и сыграли!""",
+            ),
+        ),
+    )
+    for _ in range(5)  # изменить на внешний API
+]
 
 card_user_ft = ft.Row(scroll=ft.ScrollMode.ALWAYS, controls=card_total_user)
+card_news_ft = ft.Column(scroll=ft.ScrollMode.ALWAYS, controls=card_total_news)
+ard_big_news_ft = ft.Row(wrap=True, controls=card_total_big_news)
+
 
 def main(page: ft.Page):
     page.title = "Дороги России"  # заголовок окна
     page.theme_mode = ft.ThemeMode.LIGHT
 
-
     def calc(e):
         page.open(
-                ft.DatePicker(
-                    first_date=datetime.datetime(year=2023, month=10, day=1),
-                    last_date=datetime.datetime(
-                        year=int(date.today().strftime("%Y")),
-                        month=12,
-                        day=31,
-                    )
-                )
+            ft.DatePicker(
+                first_date=datetime.datetime(year=2023, month=10, day=1),
+                last_date=datetime.datetime(
+                    year=int(date.today().strftime("%Y")),
+                    month=12,
+                    day=31,
+                ),
             )
+        )
         page.update()
 
     page.add(
@@ -92,17 +155,46 @@ def main(page: ft.Page):
                     height=130,
                     margin=10,
                 ),
-                ft.Row([
-                ft.Column([
-                    ft.Text("Календарь событий", style=ft.TextStyle(color="black", size=30)),
-                    ft.Button(text="Показать календарь",on_click=calc)
-                ]),
-                    ft.Column([
-                        ft.Text("Новости", style=ft.TextStyle(color="black", size=30)),
-                    ]),
-                    ]),
+                ft.Row(
+                    [
+                        ft.Container(
+                            ft.Column(
+                                [
+                                    ft.Text(
+                                        "Календарь событий",
+                                        style=ft.TextStyle(color="black", size=30),
+                                    ),
+                                    ft.Button(text="Показать календарь", on_click=calc),
+                                    ft.Text(
+                                        "События",
+                                        style=ft.TextStyle(color="black", size=25),
+                                    ),
+                                    ft.Container(
+                                        card_news_ft,
+                                        height=850,
+                                    ),
+                                ],
+                            ),
+                            width=300,
+                        ),
+                        ft.Container(
+                            ft.Column(
+                                [
+                                    ft.Text(
+                                        "Новости",
+                                        style=ft.TextStyle(color="black", size=30),
+                                    ),
+                                    ard_big_news_ft,
+                                ],
+                            ),
+                            width=1000,
+                        ),
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                ),
             ],
-
+            height=900,
+            scroll=ft.ScrollMode.ALWAYS,
         ),
     )
 
