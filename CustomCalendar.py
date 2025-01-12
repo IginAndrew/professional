21d86ab786d7c5c77ac5bf54ee11061f6b69fd68
IginAndrew <asigin79@gmail.com>
2025-01-12 15:58:46 +0300

    add custom calendar

--- CustomCalendar.py
+++ CustomCalendar.py
@@ -4,12 +4,17 @@ import copy
 
 
 class CustomCalendar(flet.Container):
+    # Устанавливаем текущий месяц как текущий месяц сегодняшней даты
     _current_month = datetime.date.today().month
+    # Устанавливаем текущий год как текущий год сегодняшней даты
     _current_year = datetime.date.today().year
+    # Устанавливаем текущий день как текущий день сегодняшней даты
     _current_day = datetime.date.today().day
+    # Получаем первый и последний день текущего месяца
     _first_day_in_month, _last_day_in_month = calendar.monthrange(
         _current_year, _current_month
     )
+
     _day_of_week = {0: "ПН", 1: "ВТ", 2: "СР", 3: "ЧТ", 4: "ПТ", 5: "СБ", 6: "ВС"}
     _months = {
         1: "ЯНВАРЬ",
@@ -37,6 +42,7 @@ class CustomCalendar(flet.Container):
         hover_color="#eeeeee",
         border_radius=15,
     ):
+        # Вызываем конструктор родительского класса
         super().__init__(
             # ширина виджета задаётся пользователем
             width=width,
@@ -48,15 +54,23 @@ class CustomCalendar(flet.Container):
             bgcolor=bgcolor,
             alignment=flet.alignment.center,
         )
+        # Устанавливаем цвет шрифта
         self.font_color = font_color
+        # Устанавливаем цвет шрифта для заголовка
         self.header_font_color = header_font_color
+        # Устанавливаем акцентный цвет
         self.accent_color = accent_color
+        # Устанавливаем цвет при наведении
         self.hover_color = hover_color
+        # Устанавливаем акцентный цвет шрифта
         self.font_accent_color = font_color_accent
+        # Устанавливаем размер шрифта
         self.font_size = width * 0.045
+        # Рисуем базовый виджет
         self.content = self._draw_base()
 
     def _draw_base(self):
+        # Создаем контейнер для отображения дня
         day_container = flet.Container(
             content=flet.Text(
                 value="",
@@ -72,9 +86,10 @@ class CustomCalendar(flet.Container):
             on_hover=self._on_hover_date,
             data="",
         )
+        # Создаем базовый виджет календаря
         custom_calendar = flet.Column(
             controls=[
-                # заголовок с названием месяца и годом, кнопками для перелистывания календаря
+                # Создаем строку с кнопками для перелистывания календаря и названием месяца и года
                 flet.Row(
                     controls=[
                         flet.Container(
@@ -93,7 +108,6 @@ class CustomCalendar(flet.Container):
                         ),
                         flet.Container(
                             flet.Text(
-                                # value=str(self._months.get(self._current_month)) + " ",
                                 value="",
                                 spans=[
                                     flet.TextSpan(
@@ -126,7 +140,7 @@ class CustomCalendar(flet.Container):
                     alignment=flet.MainAxisAlignment.CENTER,
                     vertical_alignment=flet.CrossAxisAlignment.START,
                 ),
-                # заголовок с днями недели
+                # Создаем строку с днями недели
                 flet.Row(
                     controls=[
                         flet.Text(
@@ -142,10 +156,9 @@ class CustomCalendar(flet.Container):
                     ],
                     alignment=flet.MainAxisAlignment.CENTER,
                     height=self.width / 10,
-                    # убирает лишние отступы между элементами Row
                     spacing=0,
                 ),
-                # пять строк на даты, создаём их и распаковываем в controls
+                # Создаем пять строк на даты
                 *[
                     flet.Row(
                         controls=[copy.deepcopy(day_container) for i in range(0, 7)],
@@ -155,34 +168,43 @@ class CustomCalendar(flet.Container):
                     for i in range(0, 5)
                 ],
             ],
-            # убирает лишние отступы между элементами Column
             spacing=0,
         )
+        # Заполняем даты в календаре
         self._fill_dates(custom_calendar)
+        # Возвращаем базовый виджет календаря
         return custom_calendar
 
     def _fill_dates(self, custom_calendar):
+        # Устанавливаем счетчик дней на 1
         day_counter = 1
+        # Получаем первый и последний день текущего месяца
         self._first_day_in_month, self._last_day_in_month = calendar.monthrange(
             self._current_year, self._current_month
         )
+        # Устанавливаем значение текста в заголовке календаря на название текущего месяца и год
         custom_calendar.controls[0].controls[1].content.value = self._months.get(
             self._current_month
         )
         custom_calendar.controls[0].controls[1].content.spans[0].text = " " + str(
             self._current_year
         )
+        # Проходим по каждой строке в календаре
         for row_id, row in enumerate(custom_calendar.controls):
-            # первые две строки это заголовки календаря
+            # Пропускаем первые две строки, так как они являются заголовками календаря
             if row_id < 2:
                 continue
+            # Проходим по каждому столбцу в строке
             for column_id, column in enumerate(row.controls):
+                # Если текущий день меньше первого дня месяца, устанавливаем значение текста в столбце на "ꟷ"
                 if row_id - 2 == 0 and column_id < self._first_day_in_month:
                     column.content.value = "ꟷ"
                     column.content.weight = flet.FontWeight.W_200
+                # Если счетчик дней меньше или равен последнему дню месяца, устанавливаем значение текста в столбце на текущий день
                 elif self._last_day_in_month >= day_counter:
                     column.content.value = str(day_counter)
                     column.content.weight = flet.FontWeight.W_500
+                    # Если текущий день равен текущему дню, месяцу и году, устанавливаем цвет фона и шрифта на акцентные цвета
                     if (
                         day_counter == self._current_day
                         and self._current_month == CustomCalendar._current_month
@@ -191,11 +213,14 @@ class CustomCalendar(flet.Container):
                         column.bgcolor = self.accent_color
                         column.content.color = self.font_accent_color
                         column.data = "today"
+                    # В противном случае устанавливаем цвет фона и шрифта на обычные цвета
                     else:
                         column.bgcolor = self.bgcolor
                         column.content.color = self.font_color
                         column.data = "just_a_day"
+                    # Увеличиваем счетчик дней на 1
                     day_counter += 1
+                # Если счетчик дней больше последнего дня месяца, устанавливаем значение текста в столбце на "ꟷ"
                 else:
                     column.content.value = "ꟷ"
                     column.content.weight = flet.FontWeight.W_200
@@ -210,19 +235,27 @@ class CustomCalendar(flet.Container):
         e.control.update()
 
     def _flip_calendar(self, e: flet.ControlEvent):
+        # Проверяем, была ли нажата кнопка "left_flip"
         if e.control.data == "left_flip":
+            # Если текущий месяц больше 1, уменьшаем его на 1
             if self._current_month - 1 > 0:
                 self._current_month = self._current_month - 1
+            # Если текущий месяц равен 1, устанавливаем его в 12 и уменьшаем год на 1
             else:
                 self._current_month = 12
                 self._current_year = self._current_year - 1
+        # Проверяем, была ли нажата кнопка "right_flip"
         elif e.control.data == "right_flip":
+            # Если текущий месяц меньше 12, увеличиваем его на 1
             if self._current_month + 1 < 12:
                 self._current_month = self._current_month + 1
+            # Если текущий месяц равен 12, устанавливаем его в 1 и увеличиваем год на 1
             else:
                 self._current_month = 1
                 self._current_year = self._current_year + 1
+        # Заполняем даты в календаре
         self._fill_dates(self.content)
+        # Обновляем содержимое календаря
         self.content.update()
 
 
