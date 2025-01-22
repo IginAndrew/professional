d596c52ac0ddef074d5d0fade89f4cad7033abcc
IginAndrew <asigin79@gmail.com>
2025-01-22 12:25:07 +0300

    сессия 2 - end

--- db.py
+++ db.py
@@ -1,20 +1,25 @@
 import sqlite3
 
+
 def get_connection():
-    c=sqlite3.connect('bd.db')
-    c.row_factory=sqlite3.Row
+    c = sqlite3.connect("bd.db")
+    c.row_factory = sqlite3.Row
     return c
 
+
 def create_table_departament():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Departament(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Departament(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT
-    )''')
+    )"""
+    )
     c.close()
 
+
 def insert_departament(name):
-    con= get_connection()
+    con = get_connection()
     with con:
         c = con.cursor()
     c.execute(
@@ -22,25 +27,27 @@ def insert_departament(name):
               INSERT INTO Departament (name) values
               (?);
               """,
-        (
-            name,
-        ),
+        (name,),
     )
     con.commit()
     return c.lastrowid
 
+
 def create_table_mini_departament():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Mini_departament(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Mini_departament(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         departament_id INTEGER,
         FOREIGN KEY (departament_id) REFERENCES Departament(id)
-    )''')
+    )"""
+    )
     c.close()
 
+
 def insert_mini_departament(name, departament_id):
-    con= get_connection()
+    con = get_connection()
     with con:
         c = con.cursor()
     c.execute(
@@ -49,24 +56,29 @@ def insert_mini_departament(name, departament_id):
               (?,?);
               """,
         (
-            name, departament_id,
+            name,
+            departament_id,
         ),
     )
     con.commit()
     return c.lastrowid
 
+
 def create_table_managment():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Managment(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Managment(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         mini_departament_id INTEGER,
         FOREIGN KEY (mini_departament_id) REFERENCES Mini_departament(id)
-    )''')
+    )"""
+    )
     c.close()
 
+
 def insert_managment(name, mini_departament_id):
-    con= get_connection()
+    con = get_connection()
     with con:
         c = con.cursor()
     c.execute(
@@ -75,15 +87,18 @@ def insert_managment(name, mini_departament_id):
               (?,?);
               """,
         (
-            name, mini_departament_id,
+            name,
+            mini_departament_id,
         ),
     )
     con.commit()
     return c.lastrowid
 
+
 def create_post():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Post(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Post(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         id_departament INTEGER,
@@ -92,11 +107,13 @@ def create_post():
         FOREIGN KEY (id_departament) REFERENCES Departament(id),
         FOREIGN KEY (id_mini_departament) REFERENCES Mini_departament(id),
         FOREIGN KEY (id_management) REFERENCES Management(id)
-    )''')
+    )"""
+    )
     c.close()
 
+
 def insert_post(name, id_department=0, id_mini_departament=0, id_management=0):
-    con= get_connection()
+    con = get_connection()
     with con:
         c = con.cursor()
     c.execute(
@@ -105,15 +122,20 @@ def insert_post(name, id_department=0, id_mini_departament=0, id_management=0):
               (?,?,?,?);
               """,
         (
-            name, id_department, id_mini_departament, id_management,
+            name,
+            id_department,
+            id_mini_departament,
+            id_management,
         ),
     )
     con.commit()
     return c.lastrowid
 
+
 def create_user():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS User(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS User(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         email TEXT,
@@ -123,11 +145,13 @@ def create_user():
         info TEXT,
         id_post INTEGER,
         FOREIGN KEY (id_post) REFERENCES Post(id)
-    )''')
+    )"""
+    )
     c.close()
 
+
 def insert_user(name, email, birthday, phone, room, info, id_post):
-    con= get_connection()
+    con = get_connection()
     with con:
         c = con.cursor()
     c.execute(
@@ -136,77 +160,104 @@ def insert_user(name, email, birthday, phone, room, info, id_post):
               (?,?,?,?,?,?,?);
               """,
         (
-            name, email, birthday, phone, room, info, id_post,
+            name,
+            email,
+            birthday,
+            phone,
+            room,
+            info,
+            id_post,
         ),
     )
     con.commit()
     return c.lastrowid
 
-#-------------------------------------
+
+# -------------------------------------
+
 
 def create_helper():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Helper(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Helper(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT
-    )''')
+    )"""
+    )
     c.close()
 
+
 def create_id_user_id_helper():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Id_user_id_helper(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Id_user_id_helper(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         id_user INTEGER,
         id_helper INTEGER,
         FOREIGN KEY (id_user) REFERENCES User(id),
         FOREIGN KEY (id_helper) REFERENCES Helper(id)
-    )''')
+    )"""
+    )
     c.close()
+
 
 def create_calendar():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Calendar(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Calendar(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         date TEXT
-    )''')
+    )"""
+    )
     c.close()
 
+
 def create_id_user_id_calendar():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Id_user_id_calendar(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Id_user_id_calendar(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         id_user INTEGER,
         id_calendar INTEGER,
         FOREIGN KEY (id_user) REFERENCES User(id),
         FOREIGN KEY (id_calendar) REFERENCES Calendar(id)
-    )''')
+    )"""
+    )
     c.close()
 
+
 def create_out():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Out(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Out(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name_out TEXT,
         info TEXT,
         id_calendar INTEGER,
         FOREIGN KEY (id_calendar) REFERENCES Calendar(id)
-    )''')
+    )"""
+    )
     c.close()
 
+
 def create_training():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Training(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Training(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name_training TEXT,
         info TEXT,
         id_calendar INTEGER,
         FOREIGN KEY (id_calendar) REFERENCES Calendar(id)
-    )''')
+    )"""
+    )
     c.close()
 
+
 def create_event():
-    c=get_connection()
-    c.execute('''CREATE TABLE IF NOT EXISTS Event(
+    c = get_connection()
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS Event(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name_event TEXT,
         type_event TEXT,
@@ -216,20 +267,165 @@ def create_event():
         info TEXT,
         id_calendar INTEGER,
         FOREIGN KEY (id_calendar) REFERENCES Calendar(id)
-    )''')
+    )"""
+    )
     c.close()
 
-if __name__ == '__main__':
-    create_table_departament()
-    create_table_mini_departament()
-    create_table_managment()
-    create_post()
-    create_user()
-    create_helper()
-    create_id_user_id_helper()
-    create_calendar()
-    create_id_user_id_calendar()
-    create_out()
-    create_training()
-    create_event()
 
+def select_departament():
+    con = get_connection()
+    with con:
+        c = con.cursor()
+        res = c.execute(
+            """
+               SELECT User.name, User.email, User.phone, Post.name as post_name, Departament.name as departament_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Departament ON Post.id_departament = Departament.id
+                WHERE Post.id_departament IN (SELECT id FROM Departament) 
+                """
+        )
+        res = res.fetchall()
+    if not res:
+        print("не найден")
+        return False
+    return res
+
+
+def select_mini_departament():
+    con = get_connection()
+    with con:
+        c = con.cursor()
+        res = c.execute(
+            """
+               SELECT User.name, User.email, User.phone, Post.name as post_name, Mini_departament.name as mini_departament_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Mini_departament ON Post.id_mini_departament = Mini_departament.id
+                WHERE Post.id_mini_departament IN (SELECT id FROM Mini_departament) 
+                """
+        )
+        res = res.fetchall()
+    if not res:
+        print("не найден")
+        return False
+    return res
+
+
+def select_managment():
+    con = get_connection()
+    with con:
+        c = con.cursor()
+        res = c.execute(
+            """
+               SELECT User.name, User.email, User.phone, Post.name as post_name, Managment.name as managment_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Managment ON Post.id_management = Managment.id
+                WHERE Post.id_management IN (SELECT id FROM Managment) 
+                """
+        )
+        res = res.fetchall()
+    if not res:
+        print("не найден")
+        return False
+    return res
+
+
+def select_admin_departament(name):
+    con = get_connection()
+    with con:
+        c = con.cursor()
+        res = c.execute(
+            """
+              SELECT User.name, User.email, User.phone, Post.name as post_name, Departament.name as departament_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Departament ON Post.id_departament = Departament.id
+                WHERE Departament.name = ?
+                UNION
+                SELECT User.name, User.email, User.phone, Post.name as post_name, Mini_departament.name as mini_departament_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Mini_departament ON Post.id_mini_departament = Mini_departament.id
+                JOIn Departament ON Mini_departament.departament_id = Departament.id
+                WHERE Departament.name = ?
+                UNION
+                SELECT User.name, User.email, User.phone, Post.name as post_name, Managment.name as managment_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Managment ON Post.id_management = Managment.id
+                JOIN Mini_departament ON Managment.mini_departament_id = Mini_departament.id
+                JOIN Departament ON Mini_departament.departament_id = Departament.id
+                WHERE Departament.name = ?
+                """,
+            (
+                name,
+                name,
+                name,
+            ),
+        )
+        res = res.fetchall()
+    if not res:
+        print("не найден")
+        return False
+    return res
+
+
+def select_mini_departament_all(name):
+    con = get_connection()
+    with con:
+        c = con.cursor()
+        res = c.execute(
+            """
+                SELECT User.name, User.email, User.phone, Post.name as post_name, Mini_departament.name as mini_departament_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Mini_departament ON Post.id_mini_departament = Mini_departament.id
+                WHERE Mini_departament.name = ?
+                UNION
+                SELECT User.name, User.email, User.phone, Post.name as post_name, Managment.name as managment_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Managment ON Post.id_management = Managment.id
+                JOIN Mini_departament ON Managment.mini_departament_id = Mini_departament.id
+                WHERE Mini_departament.name = ?
+                """,
+            (
+                name,
+                name,
+            ),
+        )
+        res = res.fetchall()
+    if not res:
+        print("не найден")
+        return False
+    return res
+
+
+def select_managment_all(name):
+    con = get_connection()
+    with con:
+        c = con.cursor()
+        res = c.execute(
+            """
+                SELECT User.name, User.email, User.phone, Post.name as post_name, Managment.name as managment_name, User.info FROM User
+                JOIN Post ON User.id_post = Post.id
+                JOIN Managment ON Post.id_management = Managment.id
+                WHERE Managment.name = ?
+                """,
+            (name,),
+        )
+        res = res.fetchall()
+    if not res:
+        print("не найден")
+        return False
+    return res
+
+
+if __name__ == "__main__":
+    # create_table_departament()
+    # create_table_mini_departament()
+    # create_table_managment()
+    # create_post()
+    # create_user()
+    # create_helper()
+    # create_id_user_id_helper()
+    # create_calendar()
+    # create_id_user_id_calendar()
+    # create_out()
+    # create_training()
+    # create_event()
+    print(select_admin_departament("1. Административный департамент"))
--- flet_app.py
+++ flet_app.py
@@ -1,4 +1,4 @@
-from unittest.mock import right
+from db import *
 
 import flet as ft
 
@@ -8,12 +8,12 @@ def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.LIGHT
 
     def card(
-        otdel: str = "jjj",
-        position: str = "dsds",
-        name: str = "dsds",
-        phone: str = "dsds",
-        mail: str = "dsds",
-        work: str = "dsds",
+        otdel: str,
+        position: str,
+        name: str,
+        phone: str,
+        mail: str,
+        work: str,
     ):
         c = ft.Card(
             ft.Column(
@@ -47,8 +47,159 @@ def main(page: ft.Page):
         return c
 
     card_total = []
-    # card_total.append(card())
     card_total_ft = ft.Column(scroll=ft.ScrollMode.ALWAYS, controls=card_total)
+
+    card_total_departament = [
+        card(
+            otdel=i["departament_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_departament()
+    ]
+
+    card_total_mini_departament = [
+        card(
+            otdel=i["mini_departament_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_mini_departament()
+    ]
+
+    card_total_managment = [
+        card(
+            otdel=i["managment_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_managment()
+    ]
+
+    card_total_admin_departament = [
+        card(
+            otdel=i["departament_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_admin_departament("1. Административный департамент")
+    ]
+
+    card_total_academia = [
+        card(
+            otdel=i["departament_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_admin_departament("2. Академия Умные дороги")
+    ]
+
+    card_total_dogovor = [
+        card(
+            otdel=i["mini_departament_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_mini_departament_all("1.2. Договорной отдел")
+    ]
+
+    card_total_obsh_otdel = [
+        card(
+            otdel=i["mini_departament_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_mini_departament_all("1.3. Общий отдел")
+    ]
+
+    card_total_license_otdel = [
+        card(
+            otdel=i["managment_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_managment_all("5.2.1. Лицензионный отдел")
+    ]
+
+    card_total_uprav_market = [
+        card(
+            otdel=i["managment_name"],
+            position=i["post_name"],
+            name=i["name"],
+            phone=i["phone"],
+            mail=i["email"],
+            work=i["info"],
+        )
+        for i in select_managment_all("5.2.2. Управление маркетинга")
+    ]
+
+    def card_road_of_russia(e):
+        card_total.clear()
+        card_total.extend(card_total_departament)
+        card_total.extend(card_total_mini_departament)
+        card_total.extend(card_total_managment)
+        card_total_ft.controls = card_total
+        page.update()
+
+    def card_admin_departament(e):
+        card_total.clear()
+        card_total.extend(card_total_admin_departament)
+        card_total_ft.controls = card_total
+        page.update()
+
+    def card_academia(e):
+        card_total.clear()
+        card_total.extend(card_total_academia)
+        card_total_ft.controls = card_total
+        page.update()
+
+    def card_dogovor(e):
+        card_total.clear()
+        card_total.extend(card_total_dogovor)
+        card_total_ft.controls = card_total
+        page.update()
+
+    def card_obsh_otdel(e):
+        card_total.clear()
+        card_total.extend(card_total_obsh_otdel)
+        card_total_ft.controls = card_total
+        page.update()
+
+    def card_license_otdel(e):
+        card_total.clear()
+        card_total.extend(card_total_license_otdel)
+        card_total_ft.controls = card_total
+        page.update()
+
+    def card_uprav_market(e):
+        card_total.clear()
+        card_total.extend(card_total_uprav_market)
+        card_total_ft.controls = card_total
+        page.update()
 
     page.add(
         ft.Column(
@@ -87,6 +238,7 @@ def main(page: ft.Page):
                                                     "Дороги России",
                                                     color="black",
                                                     bgcolor="lightgreen",
+                                                    on_click=card_road_of_russia,
                                                 ),
                                             ],
                                             alignment=ft.MainAxisAlignment.CENTER,
@@ -98,41 +250,49 @@ def main(page: ft.Page):
                                                     "Административный департамент",
                                                     color="black",
                                                     bgcolor="lightgreen",
+                                                    on_click=card_admin_departament,
                                                 ),
                                                 ft.FilledButton(
                                                     "Академия умные дороги",
                                                     color="black",
                                                     bgcolor="lightgreen",
+                                                    on_click=card_academia,
                                                 ),
                                             ],
                                             alignment=ft.MainAxisAlignment.CENTER,
                                         ),
+                                        ft.Container(height=10),
                                         ft.Row(
                                             [
                                                 ft.FilledButton(
                                                     "Договорной отдел",
                                                     color="black",
                                                     bgcolor="lightgreen",
+                                                    on_click=card_dogovor,
                                                 ),
                                                 ft.FilledButton(
                                                     "Общий отдел",
                                                     color="black",
                                                     bgcolor="lightgreen",
+                                                    on_click=card_obsh_otdel,
                                                 ),
                                             ],
                                             alignment=ft.MainAxisAlignment.CENTER,
                                         ),
+                                        ft.Container(height=10),
                                         ft.Row(
                                             [
                                                 ft.FilledButton(
                                                     "Линцензионный отдел",
                                                     color="black",
                                                     bgcolor="lightgreen",
+                                                    on_click=card_license_otdel,
                                                 ),
                                                 ft.FilledButton(
                                                     "Управление маркетинга",
                                                     color="black",
                                                     bgcolor="lightgreen",
+                                                    on_click=card_uprav_market,
                                                 ),
                                             ],
                                             alignment=ft.MainAxisAlignment.CENTER,
